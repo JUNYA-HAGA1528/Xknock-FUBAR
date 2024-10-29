4e4324f3aaf94f3086fe9ce40e4f9b0a7bfa9eb1
hagajunya <n0812034@yahoo.co.jp>
2024-10-30 08:19:31 +0900

    2024/10/30

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,17 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
+    // ハンバーガーメニュー
+    $('.js-hamburger').on('click', function () {
+        if ($('.js-hamburger').hasClass('is-open')) {
+            $('.js-drawer-menu').removeClass("is-open");
+            $(this).removeClass('is-open');
+            $('body').removeClass('no-scroll'); // スクロール無効クラスを削除
+        } else {
+            $('.js-drawer-menu').addClass("is-open");
+            $(this).addClass('is-open');
+            $('body').addClass('no-scroll'); // スクロール無効クラスを追加
+        }
+    });
 
 });
