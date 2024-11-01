33eece281051773618b62044c8cfec90405edcac
hagajunya <n0812034@yahoo.co.jp>
2024-11-01 19:35:19 +0900

    2024/10/31

--- src/js/script.js
+++ src/js/script.js
@@ -14,4 +14,26 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
         }
     });
 
+
+
+
+        // アコーディオン
+        document.querySelectorAll('.accordion__header').forEach(button => {
+            button.addEventListener('click', () => {
+                const accordionItem = button.parentElement;
+        
+                // アコーディオンの開閉
+                accordionItem.classList.toggle('active');
+        
+                // 他のアコーディオンを閉じる
+                document.querySelectorAll('.accordion__item').forEach(item => {
+                if (item !== accordionItem) {
+                    item.classList.remove('active');
+                }
+                });
+            });
+        });
+    
+    
+
 });
